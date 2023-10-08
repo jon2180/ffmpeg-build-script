13bb4d95fd0a790d3c219cb78b25d8102efb9663
jun.zhang <jun.zhang@appshahe.com>
2023-10-08 14:34:40 +0800

    build-windows

--- build-windows.sh
+++ build-windows.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# 安装好 msys2 后，把 /usr/bin/link.exe 改名，避免和 msvc 冲突，我们使用 msvc 编译工具链
+
 . ./common.sh
 
 echo "Current work directory $WORKING_DIR"
 
-# 安装好 msys2 后，把 /usr/bin/link.exe 改名，避免和 msvc 冲突，我们使用 msvc 编译工具链
+X264_PREFIX="/usr/local/x264"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$X264_PREFIX/lib/pkgconfig"
 
 function install_deps {
     # $1 1 执行安装 0 跳过
@@ -22,7 +25,6 @@ function install_deps {
 function prepare_build {
     echo "Building add installing libx264"
 
-    X264_PREFIX="/usr/local/x264"
     # 会编译前 clean
     cd $WORKING_DIR/x264 &&
         CC=cl ./configure --prefix=$X264_PREFIX --enable-shared &&
@@ -35,7 +37,7 @@ function prepare_build {
         echo "mv $X264_PREFIX/lib/libx264.dll.lib $X264_PREFIX/lib/libx264.lib"
         mv $X264_PREFIX/lib/libx264.dll.lib $X264_PREFIX/lib/libx264.lib
     fi
-    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$X264_PREFIX/lib/pkgconfig"
+
     cd $WORKING_DIR
 
     echo "Finished building add installing libx264"
@@ -43,11 +45,11 @@ function prepare_build {
 
 function build_windows {
     PREFIX="--prefix=/usr/local/ffmpeg"
-    
+
     COMMON_ARGS="--enable-x86asm --disable-doc --disable-ffplay --disable-ffprobe --disable-ffmpeg --enable-shared --disable-static --enable-avresample --enable-gpl --enable-libx264 --enable-optimizations"
     ARCH_ARGS="--arch=x86_64"
-    TOOLCHAIN_ARGS="--toolchain=msvc --cc=cl.exe -wd4828;4101;4028;4267;492 --cxx=cl.exe -wd4828;4101;4028;4267;492"
-    
+    TOOLCHAIN_ARGS="--toolchain=msvc"
+
     # 其他参数：
     #  --disable-bzlib --disable-libopenjpeg --disable-iconv --disable-zlib
     # EXTRA_ARGS="--extra-cflags=-l/usr/local/x264/include --extra-ldflags=-L/usr/loca/x264/lib"
@@ -60,7 +62,7 @@ function build_windows {
     cd $WORKING_DIR/$SOURCE &&
         git stash &&
         git checkout $BRANCH &&
-        ./configure $COMMON_ARGS $PREFIX $TOOLCHAIN_ARGS $ARCH_ARGS $EXTRA_ARGS &&
+        CC="cl.exe -wd4828;4101;4028;4267;492" CXX="cl.exe -wd4828;4101;4028;4267;492" ./configure $COMMON_ARGS $PREFIX $TOOLCHAIN_ARGS $ARCH_ARGS $EXTRA_ARGS &&
         make clean &&
         make -j 4 &&
         make install &&
