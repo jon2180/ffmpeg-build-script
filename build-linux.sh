0ce9d2176aa2201cc0a0272975e4537ef2b1a605
jun.zhang <jun.zhang@appshahe.com>
2023-10-09 13:29:19 +0800

    linux build

---
+++ build-linux.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+. ./common.sh
+
+set -e
+set -x
+
+#x264的头文件地址
+INC=""
+
+#x264的静态库地址
+LIB=""
+
+X264_SOURCE="$WORKING_DIR/x264"
+X264_OUTPUT="$WORKING_DIR/output/x264/linux$CPU"
+X264_CACHE="$WORKING_DIR/cache/x264/linux$CPU"
+
+FFMPEG_SOURCE="$WORKING_DIR/ffmpeg"
+FFMPEG_OUTPUT="$WORKING_DIR/output/ffmpeg/linux$CPU"
+FFMPEG_CACHE="$WORKING_DIR/cache/ffmpeg/linux$CPU"
+
+FFMPEG_ARGS="--disable-doc --disable-ffplay --disable-ffprobe --disable-ffmpeg --enable-shared --disable-static --enable-avresample --enable-optimizations"
+
+if [ -r "$X264_SOURCE" ]; then
+    mkdir -p $X264_CACHE
+    cd $X264_CACHE
+    TMPDIR="${TMPDIR/%\//}" $X264_SOURCE/configure \
+        --prefix=$X264_OUTPUT \
+        --disable-asm \
+        --enable-shared \
+        --enable-pic \
+        --disable-cli
+
+    make clean
+    make -j4
+    make install
+
+    INC="$INC -I$X264_OUTPUT/include"
+    LIB="$LIB -L$X264_OUTPUT/lib"
+
+    FFMPEG_ARGS="$FFMPEG_ARGS --enable-gpl --enable-libx264"
+    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$$X264_OUTPUT/lib/pkgconfig"
+    cd $WORKING_DIR
+    echo ">>>>>>编译完成 x264!<<<<<<"
+fi
+
+if [ -r "$FFMPEG_SOURCE" ]; then
+    # 先切分支
+    cd $FFMPEG_SOURCE
+    if [ `git status -s | wc -l` -gt 0 ]; then
+        git stash
+    fi
+    git checkout $BRANCH
+
+    rm -rf $FFMPEG_OUTPUT
+
+    # 开始编译
+    mkdir -p $FFMPEG_CACHE
+    cd $FFMPEG_CACHE
+    TMPDIR="${TMPDIR/%\//}" $FFMPEG_SOURCE/configure $FFMPEG_ARGS \
+        --prefix=$FFMPEG_OUTPUT \
+        --enable-x86asm \
+        --extra-cflags="$INC" \
+        --extra-ldflags="$LIB"
+
+    make clean
+    make -j4
+    make install
+    cd $WORKING_DIR
+    echo ">>>>>>编译完成 ffmpeg !<<<<<<"
+fi
